28ef9b1bbe9f281f1c8f78dce9c0946f7294a026
Mayna-B <me2be2014@gmail.com>
2023-08-08 22:40:48 -0400

    adding function descriptions

--- functions_practice.py
+++ functions_practice.py
@@ -1,9 +1,10 @@
+# A function named hello() that prints a greeting to the user. This function should accept no arguments and returns nothing.
 def hello():
     print("Hello!")
 
 hello()
 
-
+# A function named pack() that accepts three arguments, and returns a single list with the three arguments inside as list elements.
 def pack(item1, item2, item3):
     packed_list = [item1, item2, item3]
     return packed_list
@@ -11,7 +12,9 @@ def pack(item1, item2, item3):
 packed = pack("apple", "orange", "banana")
 print(packed)  
 
-
+# A function called eat_lunch(). This function should accept a list of any length, and print out a series of strings that say "First I eat __" 
+# (the first element of the list), and "Next I eat ___" (for the following elements in the list). If the list is empty, print "My lunchbox is empty!". 
+# The function should not return anything.
 def eat_lunch(food_list):
     if not food_list:
         print("My lunchbox is empty!")
